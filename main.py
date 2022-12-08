64ddd82ba1f8e2adf61ec9bad1fb7e418087f3f1
timothyplummer <timtplummer@gmail.com>
2022-12-08 11:38:56 -0500

    Day 5 Completed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import re
 
 
-def read_input_file(filepath: str, function) -> list[int]:
+def read_input_file(filepath: str, function) -> list[any]:
     with open(filepath, "r") as file1:
         Lines = file1.readlines()
         output = []
@@ -121,8 +121,9 @@ def intArrayBuilder(line):
 
 
 def day_10_2021():
-    lines = np.array(read_input_file("sample.txt", intArrayBuilder))
-    print((lines + 2) % 10)
+    octopusGrid = np.array(read_input_file("sample.txt", intArrayBuilder))
+    print((octopusGrid + 2) >= 10)
+    
 
 
 def pair_reader(line):
@@ -136,22 +137,69 @@ def pair_reader(line):
 
 def day_4():
     lines = read_input_file("Inputs/Day4.txt", pair_reader)
-    countcontains =0
-    countoverlap =0
+    countcontains = 0
+    countoverlap = 0
     for arr in lines:
         elf1 = range(arr[0], arr[1] + 1)
         elf2 = range(arr[2], arr[3] + 1)
         if (set(elf1).issubset(set(elf2))) or (set(elf2).issubset(elf1)):
             countcontains += 1
-        if (range(max(elf1[0], elf2[0]), min(elf1[-1], elf2[-1])+1)):
-            countoverlap+=1
+        if range(max(elf1[0], elf2[0]), min(elf1[-1], elf2[-1]) + 1):
+            countoverlap += 1
     print(f"number of sections contains: {countcontains}")
     print(f"number of sections overlap: {countoverlap}")
 
 
+def day_5():
+    stacks, instuctions = read_crates_file("Inputs/Day5.txt")
+    for line in instuctions:
+        matches = re.search(r"move (\d*) from (\d*) to (\d*)", line)
+        output = []
+        for m in matches.groups():
+            num = int(m)
+            output = output + [num]
+        tempStack = []
+        for i in range(int(output[0])):
+            tempStack.append(stacks[int(output[1])-1].pop())
+        print(tempStack)
+        for i in range(int(output[0])):
+                stacks[int(output[2])-1].append(tempStack.pop())
+    out = ""
+    for s in stacks:
+        out = out + s[-1]
+    print(out)
+    
+def read_crates_file(filepath:str):
+    with open(filepath, "r") as file1:
+        Lines = file1.readlines()
+        outputDrawing = []
+        outputInstuctions = []
+        afterEmptyLine = False
+        for line in Lines:
+            if line.strip() == "":
+                afterEmptyLine = True
+                continue
+            if afterEmptyLine:
+                outputInstuctions = outputInstuctions + [line]
+            else:
+                outputDrawing = outputDrawing + [line]
+    stacks = []           
+    for  i, crateNum in enumerate(outputDrawing[-1]):
+        if crateNum!=' ':
+            stack = []
+            for level in reversed(outputDrawing[0:-1]): 
+                if level[i]!= ' ' and level[i]!= "\n":
+                    stack = stack+[level[i]]
+            stacks.append(stack)    
+            print(f"{stacks[0:-1]}")
+    stacks.pop()
+    print(stacks)
+    return (stacks, outputInstuctions)
+    
 if __name__ == "__main__":
     # day_1()
     # day_2()
     # day_3()
-    day_4()
-    # day_10_2021()
+    # day_4()
+    day_5()
+    #day_10_2021()
